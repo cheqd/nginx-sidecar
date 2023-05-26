763468d810a44185e02d70e7b60568261c7ddef1
Ankur Banerjee <ankurdotb@users.noreply.github.com>
2023-05-26 16:49:56 +0100

    Change to bash since it's now in container

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
-set -ex
+set -euo pipefail
 
 # nginx.conf doesn't support environment variables,
 # so we substitute at run time
 
-/bin/sed \
+sed \
   -e "s/<API_PORT>/${API_PORT}/g" \
   -e "s|<API_ENDPOINT>|${API_ENDPOINT}|g" \
   /etc/nginx/templates/logto-app.conf.template > /etc/nginx/sites-enabled/logto-app.conf
 
-/bin/sed \
+sed \
   -e "s/<ADMIN_PORT>/${ADMIN_PORT}/g" \
   -e "s|<ADMIN_ENDPOINT>|${ADMIN_ENDPOINT}|g" \
   /etc/nginx/templates/logto-admin.conf.template > /etc/nginx/sites-enabled/logto-admin.conf
 
 # run in foreground as pid 1
-exec /usr/sbin/nginx -g 'daemon off;' -c /etc/nginx/nginx.conf
+exec nginx -g "daemon off;"
